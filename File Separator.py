3158596c34fa6fe667159d46d45ad0c8db08bc3b
Sahebul Karim <59815549+sahebulkarim@users.noreply.github.com>
2022-12-11 16:32:54 +0600

    Add files via upload

---
+++ File Separator.py
@@ -0,0 +1,22 @@
+import os
+
+# check current directory
+print(os.getcwd())
+
+# read the files from a folder
+folderpath = r"C:\Users\CIRT\PycharmProjects\File Separator\Files"
+
+# change directory to my files
+os.chdir(folderpath)
+print(os.getcwd())
+
+# check files in the folder
+print(os.listdir())
+
+# get extension
+list_extension = []
+for f in os.listdir():
+    extension = f.split(".")[-1]   # -1 last element of the list
+    list_extension.append(extension)
+
+print(list_extension)
